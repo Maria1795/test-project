f8fa564e4781fefb36fcd3cb98228613e28a3f71
mariacibrivska <tsybrivskamaria@gmail.com>
2023-12-13 23:03:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <div>
+            Hello world
+          </div>
           Learn React
         </a>
       </header>
